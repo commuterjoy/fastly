f1e2fe0a7660acddc13988df61768b195db979ba
Paul I Wilson <paul.i.wilson@ft.com>
2016-04-12 15:04:37 +0100

    adding methods for healthchecks

--- test/unit/interface.js
+++ test/unit/interface.js
@@ -1,11 +1,18 @@
 var test    = require('tap').test,
     fastly  = require(__dirname + '/../../lib/index.js');
+    methods = require(__dirname + '/../../lib/api').filter(a => a.fn).map(a => a.fn);
 
 test('unit', function (t) {
     t.type(fastly, 'function', 'module is a function');
 
     var ready = fastly('apikey');
+
     t.type(ready, 'object', 'module exposes an object');
     t.type(ready.request, 'function', 'request method exists');
+
+    methods.forEach(m => {
+        t.type(ready[m], 'function', `${m} method exists`);
+    });
+
     t.end();
 });
